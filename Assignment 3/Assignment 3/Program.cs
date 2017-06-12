a2bcaf2e4d27539b137cc35f97775eee1e45638b
gd47dayton <gd47dayton@vfs.com>
2017-06-11 17:27:22 -0700

    Added program description and tried out some basic crap.

--- Assignment 3/Assignment 3/Program.cs
+++ Assignment 3/Assignment 3/Program.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* This program is a farming simulator. It was made for GD47's Programming 1
+    Assignment 3. It should take in several forms of input from the player, 
+    things like: what to plant, what to water, what to feed, what to harvest.
+    Not 100% sure what the interface will look like, but we'll try stuff out.
+    It was made by Ali, Dayton Ruhl, Rajan.
+ */
+
 namespace Assignment_3
 {
     class Program
     {
+        static string sName;
+
         static void Main(string[] args)
         {
-            // test
+            WelcomePlayer();
+        }
+
+        static void WelcomePlayer()
+        {
+            Console.WriteLine("Welcome to NAMEISWIP, the decidedly average farming simulator.");
+            sName = Console.ReadLine();
         }
     }
 }
